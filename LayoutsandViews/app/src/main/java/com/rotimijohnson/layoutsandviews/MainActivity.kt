fc520fcb5ab88aebe2b28821384bc0d65cbf3264
DurotimiJohnson <durotimijohnson@gmail.com>
2020-10-08 17:54:24 +0100

    Using Marguee to move text

--- LayoutsandViews/app/src/main/java/com/rotimijohnson/layoutsandviews/MainActivity.kt
+++ LayoutsandViews/app/src/main/java/com/rotimijohnson/layoutsandviews/MainActivity.kt
@@ -2,10 +2,21 @@ package com.rotimijohnson.layoutsandviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var hello_word : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello_word = findViewById<TextView>(R.id.textView_1)
+        hello_word.text = "Welcome to Layouts and Views"
+        //trying to make the text view move
+        hello_word.setSingleLine()
+        hello_word.ellipsize = TextUtils.TruncateAt.MARQUEE
+        hello_word.marqueeRepeatLimit = -1
+        hello_word.isSelected = true
+
     }
 }
