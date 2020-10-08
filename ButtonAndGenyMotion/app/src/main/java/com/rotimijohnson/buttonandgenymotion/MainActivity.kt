feb5b31f96ec278c0a58a388de40ac700f9609be
DurotimiJohnson <durotimijohnson@gmail.com>
2020-10-08 18:07:33 +0100

    Button and GenyMotion

--- ButtonAndGenyMotion/app/src/main/java/com/rotimijohnson/buttonandgenymotion/MainActivity.kt
+++ ButtonAndGenyMotion/app/src/main/java/com/rotimijohnson/buttonandgenymotion/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rotimijohnson.buttonandgenymotion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Btn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Btn = findViewById<Button>(R.id.btn_click_me)
+        Btn.setOnClickListener(View.OnClickListener {
+            Btn.text = "Clicked"
+        })
+
+        Btn.setOnLongClickListener(View.OnLongClickListener {
+            Btn.text = "Long Clicked"
+            true
+        })
     }
 }
