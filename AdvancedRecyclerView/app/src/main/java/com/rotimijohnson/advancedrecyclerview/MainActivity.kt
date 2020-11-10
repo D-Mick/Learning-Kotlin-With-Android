a6775e5b451c61e9a6dd1d23d3e57fd077809321
DurotimiJohnson <durotimijohnson@gmail.com>
2020-11-10 21:03:31 +0100

    Learning Kotlin With Android:

--- AdvancedRecyclerView/app/src/main/java/com/rotimijohnson/advancedrecyclerview/MainActivity.kt
+++ AdvancedRecyclerView/app/src/main/java/com/rotimijohnson/advancedrecyclerview/MainActivity.kt
@@ -5,37 +5,50 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val itemList = generateDummyList(500)
+    private val adapter = ListAdapter(itemList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val itemList = generateDummyList(500)
-        recycler_view.adapter = ListAdapter(itemList)
+        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
 
-    fun insertItem(view: View){
+    fun insertItem(view: View) {
+        val index: Int = Random.nextInt(8)
 
+        val newItem = ListItem(
+            R.drawable.ic_check_circle,
+            "New item at position $index",
+            "Line 2"
+        )
+
+        itemList.add(index, newItem)
+        adapter.notifyItemInserted(index)
     }
 
-    fun removeItem(view: View){
-
+    fun removeItem(view: View) {
+        val index: Int = Random.nextInt(8)
+        itemList.removeAt(index)
+        adapter.notifyItemRemoved(index)
     }
 
-    private fun generateDummyList(size : Int): List<ListItem>{
+    private fun generateDummyList(size: Int): ArrayList<ListItem> {
         val list = ArrayList<ListItem>()
 
-        for(i in 0 until size){
-            val drawable = when ( i % 3){
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
                 0 -> R.drawable.ic_android_black
                 1 -> R.drawable.ic_album_
                 else -> R.drawable.ic_beenhere
             }
 
-            val item = ListItem(drawable,"Item $i","Line 2")
+            val item = ListItem(drawable, "Item $i", "Line 2")
             list += item
         }
         return list
